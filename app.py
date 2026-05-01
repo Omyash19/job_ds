eb7b48f629e7f0f6fa5631175ddd4d89265c80b0
Om madhavi <150105556+Omyash19@users.noreply.github.com>
2026-05-01 13:54:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ raw_df = load_data()
 if 'triggered' not in st.session_state: st.session_state.triggered = False
 
 # 6. APP CONTENT (ONE PAGE FLOW)
-st.markdown('<div class="master-header"><h1 class="master-title">ECHOES 💎</h1><p class="master-subtitle">High Precision Data for the Modern Analyst</p></div>', unsafe_allow_html=True)
+st.markdown('<div class="master-header"><h1 class="master-title">MarketSense</h1><p class="Advanced Analytics for Global Tech Market Discovery"</p></div>', unsafe_allow_html=True)
 
 if not raw_df.empty:
     # ─── MASTER FILTER SECTION ───
